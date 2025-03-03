6fa88fb8c21b492778b2076faf6467988110e243
kniglt12 <maojiachen2005@163.com>
2025-03-03 11:29:39 +0800

    设置返回

--- code/Setting.js
+++ code/Setting.js
@@ -6,5 +6,21 @@ export default class Menu extends Phaser.Scene {
 
     preload() {
         this.add.image(0, 0, 'setting').setOrigin(0, 0);
+        //ui
+        this.add.image(0, 0, 'ui').setOrigin(0, 0);
+        // console.log('add ui');
+        const ui1 = this.add.zone(26, 10, 80, 80).setOrigin(0, 0).setInteractive();//返回
+        const ui2 = this.add.zone(816, 0, 288, 109).setOrigin(0, 0).setInteractive();//主菜单
+        const ui3 = this.add.zone(1815, 10, 80, 80).setOrigin(0, 0).setInteractive();//设置
+
+        ui1.on('pointerdown', () => {
+            this.scene.start('Menu');
+        });
+        ui2.on('pointerdown', () => {
+            this.scene.start('Menu');
+        });
+        ui3.on('pointerdown', () => {
+            this.scene.start('Setting');
+        });
     }
 }
--- code/global.js
+++ code/global.js
@@ -1,12 +1,13 @@
-export let pt1Find = 0;
-export let pt2Find = 0;
-export let pt3Find = 0;
-export let pt4Find = 0;
-export let pt5Find = 0;
-export let pt6Find = 0;
-export let pt7Find = 0;
-export let pt8Find = 0;
-export let pt9Find = 0;
+let init = 0;
+export let pt1Find = init;
+export let pt2Find = init;
+export let pt3Find = init;
+export let pt4Find = init;
+export let pt5Find = init;
+export let pt6Find = init;
+export let pt7Find = init;
+export let pt8Find = init;
+export let pt9Find = init;
 
 export function setPt1Find(value) {
     pt1Find = value;
